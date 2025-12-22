2a1600ac0f480780a8203b2d028e9ed025d559f0
bala-amypo <bala@amypo.in>
2025-12-22 18:47:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/SERVICE IMPLEMENTATIONS/UrgencyPolicyServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.UrgencyPolicy;
+import com.example.demo.repository.UrgencyPolicyRepository;
+import com.example.demo.service.UrgencyPolicyService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UrgencyPolicyServiceImpl implements UrgencyPolicyService {
+
+    private final UrgencyPolicyRepository repository;
+
+    public UrgencyPolicyServiceImpl(UrgencyPolicyRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UrgencyPolicy createPolicy(UrgencyPolicy policy) {
+        return repository.save(policy);
+    }
+
+    @Override
+    public UrgencyPolicy getPolicy(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Policy not found"));
+    }
+
+    @Override
+    public List<UrgencyPolicy> getAllPolicies() {
+        return repository.findAll();
+    }
+}
