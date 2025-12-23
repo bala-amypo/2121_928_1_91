7f2f81a595292a035f9a037bb19fed83a867647a
bala-amypo <bala@amypo.in>
2025-12-23 05:20:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/TicketController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Ticket;
+import com.example.demo.service.TicketService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tickets")
+@Tag(name = "Tickets")
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @PostMapping
+    public Ticket createTicket(@RequestBody Ticket ticket) {
+        return ticketService.createTicket(ticket);
+    }
+
+    @GetMapping
+    public List<Ticket> getAllTickets() {
+        return ticketService.getAllTickets();
+    }
+
+    @GetMapping("/{id}")
+    public Ticket getTicket(@PathVariable Long id) {
+        return ticketService.getTicket(id);
+    }
+}
