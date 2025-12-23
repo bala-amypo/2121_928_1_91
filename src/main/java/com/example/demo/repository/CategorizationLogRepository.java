bf35951960889f942962bd575846021c51ee1fbe
bala-amypo <bala@amypo.in>
2025-12-23 09:05:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategorizationLogRepository.java
+++ src/main/java/com/example/demo/repository/CategorizationLogRepository.java
@@ -3,5 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.model.CategorizationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategorizationLogRepository extends JpaRepository<CategorizationLog, Long> {
+import java.util.List;
+
+public interface CategorizationLogRepository
+        extends JpaRepository<CategorizationLog, Long> {
+
+    List<CategorizationLog> findByTicket_Id(Long ticketId);
 }
--- src/main/java/com/example/demo/repository/UrgencyPolicyRepository.java
+++ src/main/java/com/example/demo/repository/UrgencyPolicyRepository.java
@@ -1,9 +1,7 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.*;
+import com.example.demo.model.UrgencyPolicy;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+public interface UrgencyPolicyRepository extends JpaRepository<UrgencyPolicy, Long> {
 }
