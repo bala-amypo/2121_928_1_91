1b53b6749246b11f9ea3256d2881d89182cb0205
bala-amypo <bala@amypo.in>
2025-12-26 17:29:00 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,15 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
-    // Test case line 583 requires findByEmail
+
     Optional<User> findByEmail(String email);
 
-    // Test case lines 137, 156, and 373 require existsByEmail
     boolean existsByEmail(String email);
 }
